810d54c97904c4e630c1fda4cac63aeb1841331c
ifthekar sajal <ifthekarsajal24@gmail.com>
2023-05-01 20:07:26 +0600

    sum program added

---
+++ sum.c
@@ -0,0 +1,14 @@
+#include<stdio.h>
+int main(){
+
+    int a,b,sum;
+    printf("Enter First Value::");
+    scanf("%d",&a);
+    printf("Enter Second Value::");
+    scanf("%d",&b);
+    sum=a+b;
+    printf("sum is::%d",sum);
+    
+    return 0;
+
+}
