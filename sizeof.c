a6ef7a65b6db46d16a48e2e4295768fa693c4090
ifthekar sajal <ifthekarsajal24@gmail.com>
2023-05-01 20:19:03 +0600

    sizeof function use for finding data type

---
+++ sizeof.c
@@ -0,0 +1,13 @@
+#include<stdio.h>
+int main(){
+    int a;
+    float b;
+    double c;
+    char d;
+    printf("Integer size is:: %d bytes\n" ,sizeof(a));
+     printf("Float size is:: %d bytes\n" ,sizeof(b));
+      printf("Double size is:: %d bytes\n" ,sizeof(c));
+      printf("Character size is:: %d bytes" ,sizeof(d));
+      
+
+}
