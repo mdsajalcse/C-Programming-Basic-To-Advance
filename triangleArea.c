4b22011b5ff1ded4c326afb91bf47a247ae8cfaa
ifthekar sajal <ifthekarsajal24@gmail.com>
2023-05-14 20:55:17 +0600

    area of triangle

---
+++ triangleArea.c
@@ -0,0 +1,17 @@
+
+#include<stdio.h>
+int main()
+{
+    double base ,height ,area;
+
+    printf("Enter base::");
+    scanf("%lf",&base);
+    printf("Enter height::");
+    scanf("%lf",&height);
+    area=0.5*base*height;
+    printf("Triangle area is::%lf",area);
+
+
+    return 0;
+
+}
