69c2cc59b24d2f6bad95cb4cd60146eed3c59cc9
ifthekar sajal <ifthekarsajal24@gmail.com>
2023-05-14 20:09:58 +0600

    two numbers sum program

---
+++ twoSum.c
@@ -0,0 +1,13 @@
+#include<stdio.h>
+int main()
+{
+    int num1,num2, sum;
+
+    printf("Enter Two Numbers::");
+    scanf("%d %d",&num1,&num2);
+    sum=num1+num2;
+    printf("Sum is::%d",sum);
+
+
+    return 0;
+}
