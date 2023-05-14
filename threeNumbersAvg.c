65dd114776c1df24e698b622d7bc405b61d3e6a2
ifthekar sajal <ifthekarsajal24@gmail.com>
2023-05-14 20:16:39 +0600

    sum and three numbers average value

---
+++ threeNumbersAvg.c
@@ -0,0 +1,14 @@
+#include<stdio.h>
+int main()
+{
+    int num1,num2,num3,sum_avg;
+
+    printf("Enter Three Numbers::");
+    scanf("%d %d %d",&num1,&num2,&num3);
+    sum_avg=(num1+num2+num3)/3;
+    printf("Sum Avg is::%d",sum_avg);
+
+
+    return 0;
+
+}
