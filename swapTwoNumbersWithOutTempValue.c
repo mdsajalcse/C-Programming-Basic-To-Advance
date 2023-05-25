abf3f8d1b055dff347aea41ce62f57aaa7702225
ifthekar sajal <ifthekarsajal24@gmail.com>
2023-05-25 20:55:26 +0600

    Swap two numbers with out temp value

---
+++ swapTwoNumbersWithOutTempValue.c
@@ -0,0 +1,16 @@
+#include<stdio.h>
+int main(){
+
+     int num1=10,num2=30;
+
+     num1=num2+num1;
+     num2=num1-num2;
+     num1=num1-num2;
+
+     printf("Number One =%d\n",num1);
+      printf("Number Two =%d\n",num2);
+
+
+return 0;
+
+}
