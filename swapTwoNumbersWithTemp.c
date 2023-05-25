a41040b7877b6641f18746bf874895143bdc7e70
ifthekar sajal <ifthekarsajal24@gmail.com>
2023-05-25 20:49:03 +0600

    swap two numbers using temp value

---
+++ swapTwoNumbersWithTemp.c
@@ -0,0 +1,16 @@
+#include<stdio.h>
+int main(){
+
+     int num1=10,num2=30,tempValue;
+
+     tempValue=num1;
+     num1=num2;
+     num2=tempValue;
+
+     printf("Number One =%d\n",num1);
+      printf("Number Two =%d\n",num2);
+
+
+return 0;
+
+}
